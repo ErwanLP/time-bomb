271719a51fef5577cff5f6131313c14f1d609f9d
Erwan <e.lepoder@thecodingmachine.com>
2019-02-01 13:13:07 +0100

    add router

--- pwa/src/App.vue
+++ pwa/src/App.vue
@@ -1,37 +1,82 @@
 <template>
-    <v-app>
-        <v-toolbar app>
-            <v-toolbar-title class="headline text-uppercase">
-                <span>Vuetify</span>
-                <span class="font-weight-light">MATERIAL DESIGN</span>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn
-                    flat
-                    href="https://github.com/vuetifyjs/vuetify/releases/latest"
-                    target="_blank"
-            >
-                <span class="mr-2">Latest Release</span>
-            </v-btn>
+    <v-app id="inspire">
+        <v-navigation-drawer
+                fixed
+                v-model="drawer"
+                app
+        >
+            <v-list dense>
+                <v-list-tile @click="">
+                    <v-list-tile-action>
+                        <v-icon>home</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>
+                            <router-link to="/">Home</router-link>
+                        </v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile @click="">
+                    <v-list-tile-action>
+                        <v-icon>contact_mail</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>
+                            <router-link to="/about">About</router-link>
+                        </v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar color="indigo" dark fixed app>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title>Time Bomb</v-toolbar-title>
         </v-toolbar>
-
         <v-content>
-            <HelloWorld/>
+            <v-container fluid fill-height>
+                <v-layout
+                        justify-center
+                        align-center
+                >
+                    <v-flex text-xs-center>
+                        <router-view></router-view>
+                    </v-flex>
+                </v-layout>
+            </v-container>
         </v-content>
+        <v-footer
+                height="auto"
+                color="indigo"
+        >
+            <v-layout
+                    justify-center
+                    row
+            >
+                <v-btn
+                        v-for="linkObject in links"
+                        :key="linkObject.key"
+                        color="white"
+                        flat
+                        round
+                >
+                    {{linkObject.name}}
+                </v-btn>
+            </v-layout>
+        </v-footer>
     </v-app>
 </template>
-
 <script>
-  import HelloWorld from './components/HelloWorld'
-
   export default {
     name: 'App',
-    components: {
-      HelloWorld,
-    },
+    components: {},
     data () {
       return {
-        //
+        drawer: null,
+        links: [
+          {name: 'Title 1', key: '/home'},
+          {name: 'Title 2', key: '/home2'},
+          {name: 'Title 3', key: '/home3'},
+        ],
       }
     },
   }
--- pwa/src/views/Home.vue
+++ pwa/src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
-    <HelloWorld/>
+    <v-flex xs12>
+        <v-img :aspect-ratio="16/9" :src="require('../assets/img/cover.jpg')" max-width></v-img>
+    </v-flex>
 </template>
 
 <script>
